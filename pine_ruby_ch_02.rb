b26923ae3eb639e54e52c444f31f1c65699ec1a7
Dace Willmott <dacewillmott@gmail.com>
2015-03-04 14:17:51 -0500

    add ch. 2

---
+++ pine_ruby_ch_02.rb
@@ -0,0 +1,24 @@
+# calc.rb
+
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+puts ' '
+
+# 2.5 A Few Thing to Try
+
+puts 'Hours in a year'
+puts 24 * 365
+puts ' '
+
+puts 'Minutes in a decade'
+puts 60 * 24 * 365 * 10
+puts ' '
+
+puts 'Your age in seconds'
+puts 60 * 60 * 24 * 365 * 30
+puts ' '
+
+puts 'Our dear author\'s age'
+puts (1000 * 1_025_000) / (60 * 60 *  24 * 365)
